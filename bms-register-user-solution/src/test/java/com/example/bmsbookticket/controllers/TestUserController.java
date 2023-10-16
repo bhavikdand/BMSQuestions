22d60b36bb28636a82d59150c70776bea0d8cdcf
bhavikdand <bhavik.dand_1@scaler.com>
2023-10-16 09:27:47 +0530

    Final changes

--- bms-register-user-solution/src/test/java/com/example/bmsbookticket/controllers/TestUserController.java
+++ bms-register-user-solution/src/test/java/com/example/bmsbookticket/controllers/TestUserController.java
@@ -1,7 +1,9 @@
 package com.example.bmsbookticket.controllers;
 
 import com.example.bmsbookticket.dtos.*;
+import com.example.bmsbookticket.models.User;
 import com.example.bmsbookticket.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +18,54 @@ public class TestUserController {
 
     @Autowired
     private UserController userController;
+
+    @BeforeEach
+    public void setup() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void testSignupUserSuccess() {
+        SignupUserRequestDTO requestDTO = new SignupUserRequestDTO();
+        String email = "test@scaler.com";
+        String password = "Password@123";
+        String name = "Test User";
+        requestDTO.setEmail(email);
+        requestDTO.setName(name);
+        requestDTO.setPassword(password);
+
+        SignupUserResponseDTO signupUserResponseDTO = userController.signupUser(requestDTO);
+        assertEquals(ResponseStatus.SUCCESS, signupUserResponseDTO.getResponseStatus());
+        assertEquals(email, signupUserResponseDTO.getEmail(), "Email should match");
+        assertEquals(name, signupUserResponseDTO.getName(), "Name should match");
+        User user = userRepository.findAll().stream().findFirst().orElse(null);
+        assertNotNull(user);
+        assertEquals(email, user.getEmail(), "Email should match");
+        assertEquals(name, user.getName(), "Name should match");
+        assertNotNull(user.getPassword(), "Password should not be null");
+        assertNotEquals(password, user.getPassword(), "Password should be encrypted");
+    }
+
+    @Test
+    public void testSignupUser_SameUser_RegisteringTwice_Failure() {
+        SignupUserRequestDTO requestDTO = new SignupUserRequestDTO();
+        String email = "test@scaler.com";
+        String password = "Password@123";
+        String name = "Test User";
+        requestDTO.setEmail(email);
+        requestDTO.setName(name);
+        requestDTO.setPassword(password);
+
+        SignupUserResponseDTO signupUserResponseDTO = userController.signupUser(requestDTO);
+        assertEquals(ResponseStatus.SUCCESS, signupUserResponseDTO.getResponseStatus(), "Response status should be SUCCESS");
+        assertEquals(email, signupUserResponseDTO.getEmail(), "Email should match");
+        assertEquals(name, signupUserResponseDTO.getName(), "Name should match");
+
+        signupUserResponseDTO = userController.signupUser(requestDTO);
+        assertEquals(ResponseStatus.FAILURE, signupUserResponseDTO.getResponseStatus(), "Response status should be FAILURE");
+        assertNull(signupUserResponseDTO.getEmail(), "Email should be null");
+        assertNull(signupUserResponseDTO.getName(), "Name should be null");
+    }
 
     @Test
     public void testSignupUserSuccess_AndLoginSuccess() {
@@ -38,8 +88,6 @@ public class TestUserController {
         LoginResponseDto loginResponseDto = userController.login(loginRequestDto);
         assertTrue(loginResponseDto.isLoggedIn());
         assertEquals(ResponseStatus.SUCCESS, loginResponseDto.getResponseStatus());
-
-        userRepository.deleteAll();
     }
 
     @Test
@@ -63,8 +111,6 @@ public class TestUserController {
         LoginResponseDto loginResponseDto = userController.login(loginRequestDto);
         assertFalse(loginResponseDto.isLoggedIn());
         assertEquals(ResponseStatus.SUCCESS, loginResponseDto.getResponseStatus());
-
-        userRepository.deleteAll();
     }
 
     @Test
@@ -85,7 +131,6 @@ public class TestUserController {
         assertEquals(ResponseStatus.FAILURE, signupUserResponseDTO.getResponseStatus());
         assertNull(signupUserResponseDTO.getEmail());
         assertNull(signupUserResponseDTO.getName());
-        userRepository.deleteAll();
     }
 
     @Test
@@ -99,6 +144,5 @@ public class TestUserController {
         LoginResponseDto loginResponseDto = userController.login(loginRequestDto);
         assertFalse(loginResponseDto.isLoggedIn());
         assertEquals(ResponseStatus.FAILURE, loginResponseDto.getResponseStatus());
-        userRepository.deleteAll();
     }
 }
--- bms-register-user-starter/src/main/java/com/example/bmsbookticket/models/User.java
+++ bms-register-user-starter/src/main/java/com/example/bmsbookticket/models/User.java
@@ -1,9 +1,5 @@
 package com.example.bmsbookticket.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
