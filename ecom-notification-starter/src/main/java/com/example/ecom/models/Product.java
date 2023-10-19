6a87913eec01fb8d30cc5bbf9e4df5fdd949d112
bhavikdand <bhavik.dand_1@scaler.com>
2023-10-20 00:49:30 +0530

    Removed annotations from models + added code back to InventoryController

--- ecom-notification-starter/src/main/java/com/example/ecom/models/Product.java
+++ ecom-notification-starter/src/main/java/com/example/ecom/models/Product.java
@@ -1,10 +1,8 @@
 package com.example.ecom.models;
 
-import jakarta.persistence.Entity;
 import lombok.Data;
 
 @Data
-@Entity(name = "products")
 public class Product extends BaseModel{
     private String name;
     private String description;
--- ecom-notification-starter/src/main/java/com/example/ecom/models/User.java
+++ ecom-notification-starter/src/main/java/com/example/ecom/models/User.java
@@ -1,10 +1,8 @@
 package com.example.ecom.models;
 
-import jakarta.persistence.Entity;
 import lombok.Data;
 
 @Data
-@Entity(name = "users")
 public class User extends BaseModel{
     private String name;
     private String email;
