c048dc2e65f10100454fc4bc03d231e33169b525
bhavikdand <bhavik.dand_1@scaler.com>
2023-12-06 19:57:03 +0530

    Scaler base

--- scaler-base-solution/src/main/java/com/example/ecom/ScalerApplication.java
+++ scaler-base-solution/src/main/java/com/example/ecom/ScalerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcomPlaceOrder {
+public class ScalerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcomPlaceOrder.class, args);
+        SpringApplication.run(ScalerApplication.class, args);
     }
 
 }
